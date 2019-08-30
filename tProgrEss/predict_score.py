8234150fe2a92873d7bb972ca9645a7ba22f3179
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2019-08-30 14:10:30 +0900

    div into functions

--- tProgrEss/predict_score.py
+++ tProgrEss/predict_score.py
@@ -11,32 +11,31 @@ from fbprophet.plot import plot_plotly
 students = open('./studentlist.csv')
 contests = open('./contestlist.csv')
 
-#for student in csv.reader(students):
 
-df = pd.read_csv('./score/s17t285.csv', nrows=4)
-data = pd.read_csv('./score/s17t285.csv')
-#df = pd.read_csv('./W01_C01/s16t229.csv')
+def prediction(df):
+    model = Prophet(weekly_seasonality=False, yearly_seasonality=False)
+    model.fit(df)
+    future = model.make_future_dataframe(periods=6, freq='W')
+    forecast = model.predict(future)
+    print(forecast['ds'])
+    return forecast
+    
+def each_student(students):
+    for student in csv.reader(students):
+        df = pd.read_csv('./score/'+student[0]+'.csv', nrows=4)
+        student_data = pd.read_csv('./score/'+student[0]+'.csv')
+        df['y'] = df['score']
+        student_data['ds'] = pd.to_datetime(student_data['ds'], errors='coerce')
 
-#df['ds'] = df[df['past_time_from_start'] < 90].submit_time
-#df['y'] = df[df['past_time_from_start'] < 90].score
-df['y'] = df['score']
-data['yhat'] = data['score']
-data['yhat_lower'] = data['score']
-data['yhat_upper'] = data['score']
-data['ds'] = pd.to_datetime(data['ds'], errors='coerce')
-#data.plot()
-#xfmt = mdates.DateFormatter("%y/%m/%d")
-#data.xaxis.set_major_formatter(xfmt)
+        forecast = prediction(df)
+        plot_data(forecast, student_data)
+    
 
-#print(df['ds'])
+def plot_data(forecast, actual_data):
+    plt.plot(forecast['ds'], forecast['yhat'], '-o', color='blue', label='prediction')
+    plt.plot(actual_data['ds'] ,actual_data['score'], '-o', color='red', label='actual data')
+    plt.legend(loc='upper left')
+    plt.grid()
+    plt.show()
 
-model = Prophet(weekly_seasonality=False, yearly_seasonality=False)
-model.fit(df)
-
-future = model.make_future_dataframe(periods=6, freq='W')
-forecast = model.predict(future)
-print(forecast)
-fig = model.plot(forecast)
-model.plot(data)
-#model.plot(data['ds'], data['score'])
-plt.show()
+each_student(students)
